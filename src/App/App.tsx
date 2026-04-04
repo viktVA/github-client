a3e50d21bf62b3a4e929949830ccb29a07292f96
Barbie <vika50rus@gmail.com>
2026-04-04 15:13:33 +0300

    fix(App): migrate App.css to CSS Modules

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import {Outlet, useNavigate} from "react-router-dom";
-import './App.css'
+import styles from './App.module.scss'
 import Text from "@components/Text";
 import logo from '@assets/icons/logo_github.png';
 import avatar from '@assets/icons/user_avatar.png';
@@ -10,18 +10,18 @@ function App() {
     const navigate = useNavigate();
     return (
         <div className={"app"}>
-            <div className={classNames('app__header','header')}>
-                <div className={'header__brand'} onClick={() => navigate('/repositories?page=1')}>
-                    <div className={'header__logo'}
+            <div className={classNames(styles['app__header'],styles['header'])}>
+                <div className={styles['header__brand']} onClick={() => navigate('/repositories?page=1')}>
+                    <div className={styles['header__logo']}
                         >
                         <img src={logo}/>
                     </div>
-                    <div className={'header__name'}>
+                    <div className={styles['header__name']}>
                         <Text view={'p-20'} color={'primary'} weight={'bold'}>GitHub Client</Text>
                     </div>
                 </div>
 
-                <div className={'header__user-avatar'}>
+                <div className={styles['header__user-avatar']}>
                     <img src={avatar}/>
                 </div>
             </div>
