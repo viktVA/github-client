9e3fd2b6bc22f603f08bbcfa5d28bd4fccca25b6
Barbie <vika50rus@gmail.com>
2026-04-05 19:25:12 +0300

    fix(App): fix CSS Modules class references in App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,14 +3,14 @@ import styles from './App.module.scss'
 import Text from "@components/Text";
 import logo from '@assets/icons/logo_github.png';
 import avatar from '@assets/icons/user_avatar.png';
-import classNames from "classnames";
+
 
 
 function App() {
     const navigate = useNavigate();
     return (
-        <div className={"app"}>
-            <div className={classNames(styles['app__header'],styles['header'])}>
+        <div className={styles["app"]}>
+            <div className={styles['app__header']}>
                 <div className={styles['header__brand']} onClick={() => navigate('/repositories?page=1')}>
                     <div className={styles['header__logo']}
                         >
